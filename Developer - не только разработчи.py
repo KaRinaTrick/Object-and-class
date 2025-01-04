c55792156dfa7612079af33a8dde8d58ab090ca4
KaRinaTrick <proxorova.karina36@gmail.com>
2025-01-04 19:47:55 +0300

    Add files via upload

---
+++ Developer - не только разработчи.py
@@ -0,0 +1,26 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        # if 1 < new_floor < self.number_of_floors:
+        #     for i in range (0, new_floor):
+        #         i+=1
+        #         print(i)
+        # else:
+        #     print('Такого этажа не существует')
+
+        if 1 < new_floor < self.number_of_floors:
+            i = 0
+            while i < new_floor:
+                i += 1
+                print(i)
+        else:
+            print('Такого этажа не существует')
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
